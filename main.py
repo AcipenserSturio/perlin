db2802a0921a4c08103b07b28d9a23a041ffe2b3
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2022-10-10 11:50:12 +0300

    Wrap perlin noise generation

--- main.py
+++ main.py
@@ -16,30 +16,6 @@ class Timer:
 
 timer = Timer()
 
-shape = (512,512)
-scale = 0.1
-octaves = 5
-persistence = 0.5
-lacunarity = 2.0
-seed = np.random.randint(0,100)
-
-world = np.zeros(shape)
-
-# make coordinate grid on [0,1]^2
-x_idx = np.linspace(0, 1, shape[0])
-y_idx = np.linspace(0, 1, shape[1])
-world_x, world_y = np.meshgrid(x_idx, y_idx)
-
-# apply perlin noise, instead of np.vectorize, consider using itertools.starmap()
-world = np.vectorize(noise.pnoise2)(world_x/scale,
-                        world_y/scale,
-                        octaves=octaves,
-                        persistence=persistence,
-                        lacunarity=lacunarity,
-                        repeatx=1024,
-                        repeaty=1024,
-                        base=seed)
-
 deep_blue = [0, 95, 215]
 blue = [0, 135, 255]
 green = [0, 135, 0]
@@ -47,12 +23,40 @@ beach = [255, 215, 175]
 snow = [255, 255, 255]
 mountain = [138, 138, 138]
 
+def perlin_noise (
+        shape = (512,512),
+        scale = 0.1,
+        octaves = 5,
+        persistence = 0.5,
+        lacunarity = 2.0,
+        seed = np.random.randint(0,100),
+    ):
+
+    world = np.zeros(shape)
+
+    # make coordinate grid on [0,1]^2
+    x_idx = np.linspace(0, 1, shape[0])
+    y_idx = np.linspace(0, 1, shape[1])
+    world_x, world_y = np.meshgrid(x_idx, y_idx)
+
+    # apply perlin noise, instead of np.vectorize, consider using itertools.starmap()
+    pnoise = np.vectorize(noise.pnoise2)
+    world = pnoise(world_x/scale,
+                   world_y/scale,
+                   octaves=octaves,
+                   persistence=persistence,
+                   lacunarity=lacunarity,
+                   repeatx=1024,
+                   repeaty=1024,
+                   base=seed)
+    return world
+
 def add_color(world):
     color_world = np.zeros(world.shape+(3,), dtype=np.uint8)
-    center_x, center_y = shape[1] // 2, shape[0] // 2
+    center_x, center_y = world.shape[1] // 2, world.shape[0] // 2
 
-    for y in range(shape[0]):
-        for x in range(shape[1]):
+    for y in range(world.shape[0]):
+        for x in range(world.shape[1]):
 
             perlin = linear(world[x][y], -.5, .5, -1, 1)
 
@@ -95,6 +99,7 @@ def add_color(world):
 
 
 # img = np.floor((world + .5) * 255).astype(np.uint8) # <- Normalize world first
+world = perlin_noise()
 color_world = add_color(world)
 
 timer("end")
